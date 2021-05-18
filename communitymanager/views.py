6f1f6b8893079633df7c6d03b608102b3acfcb10
Eliott Bourget <eliott.bourget@laposte.net>
2021-05-18 11:20:11 +0200

    adding templates and basic navigation

--- communitymanager/views.py
+++ communitymanager/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.contrib.auth import authenticate, login
 from . forms import ConnexionForm
 
 
+@login_required()
+def home(request):
+    return render(request, 'communitymanager/home.html')
 
 
